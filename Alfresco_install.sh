4a044786a67f5f83d926f8abaec824ba391573b8
Adil B <80790342+Foolek@users.noreply.github.com>
2024-05-27 10:32:36 +0200

    Update Alfresco_install.sh

--- Alfresco_install.sh
+++ Alfresco_install.sh
@@ -53,81 +53,80 @@ echoblue   "L'installation via ce script requiert des privilèges administrateur
 echoblue   "Tous les paquets seront installés dans /opt/. , des liens symboliques seront créés dans /usr/local/bin"
 echo
 echored    "-----------------------------------------------------"
-echo 
+echo
 echoblue   "Voulez-vous continuer et lancer l'installation ? Y/N : " 
 read accordInstallation
 echo
 
-if [ "$accordInstallation" = "y" || "$accordInstallation" = "Y" ] 
-then 
-
-          ##################################################################
-          ##### Déclaration des variables d'environnement nécessaires  #####
-          ##################################################################
-
-    sudo echo >> /etc/profile.d/alfresco_env.sh "#!/bin/bash
+if [ "$accordInstallation" = "y"] 
+    then
+        ##################################################################
+        ##### Déclaration des variables d'environnement nécessaires  #####
+        ##################################################################
     
+        sudo echo >> /etc/profile.d/alfresco_env.sh "#!/bin/bash
         
-    #####  Java variables  #####
-    export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-    export PATH=$JAVA_HOME/bin:$PATH
+            
+        #####  Java variables  #####
+        export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+        export PATH=$JAVA_HOME/bin:$PATH
+        
+        #####  Alfresco variables  #####
+        export ALF_HOME=/opt/alfresco
+        export ALF_DATA_HOME=/opt/alfresco/tomcat/data
+        export ALF_USER=alfresco
+        export ALF_GROUP=alfresco
+        
+        #####  Alfresco search services variables  #####
+        export ALF_SEARCH_HOME=$ALF_HOME/alfresco-search-services
+        
+        #####  ActiveMQ variables  #####
+        export ACTIVEMQ_HOME=$ALF_HOME/activemq
+        
+        #####  Tomcat variables  #####
+        export CATALINA_HOME=$ALF_HOME/tomcat
+        export CATALINA_BASE=$CATALINA_HOME
+        
+        #####  Solr variables  #####
+        export SOLR_HOME=$ALF_SEARCH/solrhome"
     
-    #####  Alfresco variables  #####
-    export ALF_HOME=/opt/alfresco
-    export ALF_DATA_HOME=/opt/alfresco/tomcat/data
-    export ALF_USER=alfresco
-    export ALF_GROUP=alfresco
     
-    #####  Alfresco search services variables  #####
-    export ALF_SEARCH_HOME=$ALF_HOME/alfresco-search-services
     
-    #####  ActiveMQ variables  #####
-    export ACTIVEMQ_HOME=$ALF_HOME/activemq
-    
-    #####  Tomcat variables  #####
-    export CATALINA_HOME=$ALF_HOME/tomcat
-    export CATALINA_BASE=$CATALINA_HOME
-    
-    #####  Solr variables  #####
-    export SOLR_HOME=$ALF_SEARCH/solrhome"
-
-
-
-          ###################################################################
-          #####  Installation et téléchargement des paquets nécessaires #####
-          ###################################################################
-    
-    echoblue "les paquets suivants vont être installés : git, curl, mariadb-server, openjdk-17-jdk-headless nginx ainsi que les répertoires d'Alfresco, ActiveMQ, Apache Tomcat etc.."
-    echoblue "Voulez-vous les installer ? Y/N : " 
-    read response
-    
-    if [ "$reponse" = "y" || "$reponse" = "Y" ]
-        then
-          sudo apt update -y && sudo apt upgrade -y
-          sudo apt install git curl mariadb-server openjdk-17-jdk-headless nginx -y
+        ###################################################################
+        #####  Installation et téléchargement des paquets nécessaires #####
+        ###################################################################
+        
+        echoblue "les paquets suivants vont être installés : git, curl, mariadb-server, openjdk-17-jdk-headless nginx ainsi que les répertoires d'Alfresco, ActiveMQ, Apache Tomcat etc.."
+        echoblue "Voulez-vous les installer ? Y/N : " 
+        read response
+        
+        if [ "$reponse" = "y" ]
+            then
+              sudo apt update -y && sudo apt upgrade -y
+              sudo apt install git curl mariadb-server openjdk-17-jdk-headless nginx -y
+        else
+          echo "Annulation de l'installation"
+          sudo rm /etc/profile.d/alfresco_env.sh
+        fi
+        
+        
+        #####  Variables des liens de téléchargements
+        
+        AlfContentServiceUrl=https://nexus.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/alfresco-content-services-community-distribution/23.2.1/alfresco-content-services-community-distribution-23.2.1.zip
+        
+        AlfSearchServiceUrl=https://nexus.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/alfresco-search-services/2.0.9.1/alfresco-search-services-2.0.9.1.zip
+        
+        ActiveMqUrl=https://dlcdn.apache.org//activemq/6.1.2/apache-activemq-6.1.2-bin.tar.gz
+        
+        ApacheTomcatUrl=https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.24/bin/apache-tomcat-10.1.24.zip
+        
     else
-      echo "Annulation de l'installation"
-      sudo rm /etc/profile.d/alfresco_env.sh
-    fi
+        exit
+    fi    
+        
+        
+        
+        
     
     
-    #####  Variables des liens de téléchargements
-    
-    AlfContentServiceUrl=https://nexus.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/alfresco-content-services-community-distribution/23.2.1/alfresco-content-services-community-distribution-23.2.1.zip
-    
-    AlfSearchServiceUrl=https://nexus.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/alfresco-search-services/2.0.9.1/alfresco-search-services-2.0.9.1.zip
-    
-    ActiveMqUrl=https://dlcdn.apache.org//activemq/6.1.2/apache-activemq-6.1.2-bin.tar.gz
-    
-    ApacheTomcatUrl=https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.24/bin/apache-tomcat-10.1.24.zip
-    
-else
-    exit
-fi    
-    
-    
-    
-    
-
-
 
