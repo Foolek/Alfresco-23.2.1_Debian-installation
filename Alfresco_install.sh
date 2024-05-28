ccf40a7b08142d0e6bae4f7421563ebaa04b85d8
Adil <adil.bouzit@gmail.com>
2024-05-28 23:06:47 +0200

    update

--- Alfresco_install.sh
+++ Alfresco_install.sh
@@ -417,7 +417,9 @@ if [ "$accordInstallation" = "y" ]
         smart.folders.model=alfresco/model/smartfolder.xml
         smart.folders.model.labels=alfresco/messages/smartfolder-model"
 
-        sed -i "299s/^.$/export JAVA_TOOL_OPTIONS=\"-Dencryption.keystore.type=JCEKS -Dencryption.cipherAlgorithm=DESede\/CBC\/PKCS5Padding -Dencryption.keyAlgorithm=DESede -Dencryption.keystore.location=\$CATALINA_HOME\/data\/keystore\/keystore -Dmetadata-keystore.password=mp6yc0UD9e -Dmetadata-keystore.aliases=metadata -Dmetadata-keystore.metadata.password=oKIWzVdEdA -Dmetadata-keystore.metadata.algorithm=DESede\"/" $CATALINA_HOME/bin/catalina.sh
+
+        JAVA_TOOL_OPTIONS_STRING="export JAVA_TOOL_OPTIONS=\"-Dencryption.keystore.type=JCEKS -Dencryption.cipherAlgorithm=DESede/CBC/PKCS5Padding -Dencryption.keyAlgorithm=DESede -Dencryption.keystore.location=/opt/alfresco/tomcat/data/keystore/keystore -Dmetadata-keystore.password=mp6yc0UD9e -Dmetadata-keystore.aliases=metadata -Dmetadata-keystore.metadata.password=oKIWzVdEdA -Dmetadata-keystore.metadata.algorithm=DESede\""
+        sed -i "299i $JAVA_TOOL_OPTIONS_STRING" $CATALINA_HOME/bin/catalina.sh
         
 else
     echo "opération annulée"
