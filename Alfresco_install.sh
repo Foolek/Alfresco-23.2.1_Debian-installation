02945de54e3c13a5b1c90ec7f7c5fccb1be0aec2
Adil <adil.bouzit@gmail.com>
2024-05-28 13:08:13 +0200

    Update Alfresco_install.sh

--- Alfresco_install.sh
+++ Alfresco_install.sh
@@ -335,9 +335,10 @@ if [ "$accordInstallation" = "y" ]
         rm -rf $ALF_HOME/ssl-tool
         
         
-        sudo mariadb <<< "CREATE DATABASE alfresco_db CHARACTER SET utf8 COLLATE utf8_general_ci;"
-        sudo mariadb <<< "GRANT ALL PRIVILEGES ON alfresco_db.* TO alfresco_user@localhost IDENTIFIED BY 'alfresco_password;"
-        sudo mariadb <<< "FLUSH PRIVILEGES;"
+        mariadb <<< "CREATE DATABASE alfresco_db CHARACTER SET utf8 COLLATE utf8_general_ci;"
+        mariadb <<< "CREATE USER alfresco_user@localhost IDENTIFIED BY 'alfresco_password;"
+        mariadb <<< "GRANT ALL ON alfresco_db TO alfresco_user@localhost IDENTIFIED BY 'alfresco_password'
+        mariadb <<< "FLUSH PRIVILEGES;"
         
         
         userdel $ALF_USER
