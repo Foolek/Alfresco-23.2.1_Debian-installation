206d1173f2c258e9b53e9a7898d8cf6851ea094c
Adil <adil.bouzit@gmail.com>
2024-05-28 12:06:31 +0200

    Update Alfresco_install.sh

--- Alfresco_install.sh
+++ Alfresco_install.sh
@@ -339,7 +339,7 @@ if [ "$accordInstallation" = "y" ]
         useradd $ALF_USER
         groupadd $ALF_GROUP
         passwd "$ALF_USER" <<< "$ALF_USER_PASS"
-        usermod alfresco:alfresco $ALF_HOME -r 
+        chown alfresco:alfresco $ALF_HOME -r 
 else
     echo "opération annulée"
     exit
