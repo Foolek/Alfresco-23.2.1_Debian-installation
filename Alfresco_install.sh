1408e235a99b25ee51dbfd01510ba9839d095aaf
Adil <adil.bouzit@gmail.com>
2024-05-31 12:46:00 +0200

    Update Alfresco_install.sh

--- Alfresco_install.sh
+++ Alfresco_install.sh
@@ -482,7 +482,7 @@ then
                 $Alf_user="alfresco_user"
                 $Alf_db_password="alfresco_password"
                 break
-            else if [ "$reponse" == "n" ] then
+            elif [ "$reponse" == "n" ] then
                 echogreen "Choose a name for the database : "
                 read $Alf_db
                 echogreen "Choisissez un nom pour l'utilisateur de la base de donnée d'Alfresco : "
